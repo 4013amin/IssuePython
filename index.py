a04bc0a57a514406fec371e4d40fe0e85389f6a0
delta <amin138400138400@gmail.com>
2025-03-22 20:47:49 +0330

    add Calculate

--- index.py
+++ index.py
@@ -13,13 +13,24 @@
 # print("The sum of even numbers is:", result)
 
 
-def Sum_odd_number():
-    number = int(input("Enter a number :"))
-    count = 0
-    for i in range(1 , number + 1 , 2):
-        if i % 2 != 0:
-            count += i 
-    return count
+# def Sum_odd_number():
+#     number = int(input("Enter a number :"))
+#     count = 0
+#     for i in range(1 , number + 1 , 2):
+#         if i % 2 != 0:
+#             count += i 
+#     return count
 
-result = Sum_odd_number()
-print("This is Odd number is :" , result)
+# result = Sum_odd_number()
+# print("This is Odd number is :" , result)
+
+def Calculate():
+    number = int(input("Enter a number :"))
+    
+    if number % 2 == 0 :
+         print("این عدد زوج است ")
+    else :
+         print("این عدد فرد است")
+            
+            
+Calculate()

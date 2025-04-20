5914d098062672b20cfdeadb911e1c17d1ab0d86
delta <amin138400138400@gmail.com>
2025-04-20 15:31:46 +0330

    Edit

--- index.py
+++ index.py
@@ -129,7 +129,7 @@ def multiple():
     
     total = 0 
     for i in range(1 , 1000):
-        if i % 3 == 0 or i % 5 == 0 :
+        if i % 3 == 0 or i % 5 == 0 :   
             total += i
     print(total)
         

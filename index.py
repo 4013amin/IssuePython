60d7332a4d445876e4c42519d80aa3d8e78dae56
delta <amin138400138400@gmail.com>
2025-11-07 13:39:07 +0330

    update in index.py

--- index.py
+++ index.py
@@ -1,22 +1,35 @@
-numbers = list(map(int, input("Enter numbers separated by comma: ").split(',')))
+# numbers = list(map(int, input("Enter numbers separated by comma: ").split(',')))
 
-zoj = []
-fard = []
+# zoj = []
+# fard = []
 
-for i in numbers:
-    if i % 2 == 0:
-        zoj.append(i)
-    else:
-        fard.append(i)
+# for i in numbers:
+#     if i % 2 == 0:
+#         zoj.append(i)
+#     else:
+#         fard.append(i)
 
-print("اعداد زوج", zoj)
-print("اعداد فرد ", fard)
+# print("اعداد زوج", zoj)
+# print("اعداد فرد ", fard)
 
-result_zoj = sum(zoj)
-result_fard = sum(fard)
+# result_zoj = sum(zoj)
+# result_fard = sum(fard)
 
-print("مجموع اعداد زوج " , result_zoj)
-print("مجموع اعداد فرد " , result_fard)
+# print("مجموع اعداد زوج " , result_zoj)
+# print("مجموع اعداد فرد " , result_fard)
 
 
 
+# #جدا سازی اعداد تکراری 
+# def Remove_duplicates(add):
+#     add = input("Enter numbers separated by comma: ").split(',')
+    
+#     delete_dup = set(add)
+#     return delete_dup
+
+# print(Remove_duplicates(numbers))
+
+#تبدیل عدد به متن 
+numner = 1,2,3,4,5,6,7,8,9,10
+txt = str(numner)
+print(txt)

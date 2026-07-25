6c28ee4752cfebe0547ac045c463a4637236b31e
4013amin <amin138400138400@gmail.com>
2026-07-26 00:02:02 +0330

    add backend

--- ProjectDj/app/views.py
+++ ProjectDj/app/views.py
@@ -7,19 +7,3 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = models.Task.objects.all()
     serializer_class = TaskSerializer
-
-class TaskAPIView(viewsets.ModelViewSet):
-    queryset = models.Task.objects.all()
-
-
-from rest_framework import viewsets
-from .models import Task
-from .serializers import TaskSerializer
-
-
-class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
